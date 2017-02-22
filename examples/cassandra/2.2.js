774be65fc6f950fb58c29821d557bc0edfa3fb45
Nick Riley <nriley.aytch@gmail.com>
2017-02-22 11:35:34 -0800

    Update 2.2.js

--- examples/cassandra/2.2.js
+++ examples/cassandra/2.2.js
@@ -3,7 +3,6 @@ module.exports = function (client) {
         var query = 'create table foo (bar int primary key)';
         client.db.execute(query, function(err, results) {
             if (err) {
-                console.log(err);
                 return reject(new Error('Error applying migration '));
             }
             resolve(results);
